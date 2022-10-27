4b84560cfdd182f51f049be575a4393f833637af
Gustavo-MA <111370930+Gustavo-MA@users.noreply.github.com>
2022-10-27 10:54:00 -0500

    mpn
    instalacion des y update

---
+++ Paquetes/mongodb.js
@@ -0,0 +1,11 @@
+"mongodb":{
+    "version": "4.1.3",
+    "resolved": "https://registry.npmjs.org/mongodb/-/mongodb-4.1.3.tgz",
+    "integrity": "sha512-lHvTqODBiSpuqjpCj48DOyYWS6Iq6ElJNUiH9HWdQtONyOfjgsKzJULipWduMGsSzaNO4nFi/kmlMFCLvjox/Q==",
+    "requires": {
+    "bson": "^4.5.2",
+    "denque": "^2.0.1",
+    "mongodb-connection-string-url": "^2.0.0",
+    "saslprep": "^1.0.3"
+     }
+    }
