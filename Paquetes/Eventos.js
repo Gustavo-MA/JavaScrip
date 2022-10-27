96ac868a0e3601290b2552edf01ea4c6529d9c0d
Gustavo-MA <111370930+Gustavo-MA@users.noreply.github.com>
2022-10-27 11:38:01 -0500

    EyC
    Eventos y CallBacks

---
+++ Paquetes/Eventos.js
@@ -0,0 +1,10 @@
+var eventos = require('events'); 
+var EmisorEventos = eventos.EventEmitter; 
+var manejador = new EmisorEventos(); 
+manejador.on('saludo', function(nombre){ 
+   console.log("Hola " + nombre); 
+}); 
+manejador.emit('saludo', "Gustavo"); 
+setInterval(function(){ 
+   manejador.emit('saludo',"Gustavo"); 
+},500);  
